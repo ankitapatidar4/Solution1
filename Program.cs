abbc1fd8ccee37c4166ec251d138c2a4b87da7b9
Deepanshu Mahajan <dmahajan@usf.edu>
2020-01-21 16:06:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's see the change");
+            Console.ReadLine();
+        }
+    }
+}
